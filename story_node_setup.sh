764a80fc6bdcd08e5f298640965a37c5a61c5342
s3unghyeon <user@userui-MacBookPro.local>
2024-10-15 17:38:08 +0900

    refactor : del check logs, sync

--- story_node_setup.sh
+++ story_node_setup.sh
@@ -46,7 +46,6 @@ LimitNOFILE=65535
 WantedBy=multi-user.target
 EOF
 
-    # Step 10: Create and Configure systemd Service for Cosmovisor (Story)
     sudo tee /etc/systemd/system/story.service > /dev/null <<EOF
 [Unit]
 Description=Cosmovisor service for Story binary
@@ -70,7 +69,6 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-    # Step 11: Reload systemd, Enable, and Start Services
     sudo systemctl daemon-reload
     sudo systemctl enable story-geth story
     sudo systemctl start story-geth story
@@ -78,38 +76,4 @@ EOF
     echo -e "installed successfully"
 
 }
-
-check_logs() {
-    echo -e "\nCheck Story logs"
-    sudo journalctl -u story -o cat -n 50
-    echo -e "\nCheck Story-Geth logs"
-    sudo journalctl -u story-geth -o cat -n 50
-}
-
-check_sync_status() {
-    echo -e "\nCheck node sync status..."
-    local_height=$(curl -s localhost:26657/status | jq -r '.result.sync_info.latest_block_height')
-    network_height=$(curl -s https://story-testnet-rpc.blockhub.id/status | jq -r '.result.sync_info.latest_block_height')
-    blocks_left=$((network_height - local_height))
-
-    echo -e "\033[1;32mYour node height:\033[0m \033[1;34m$local_height\033[0m" \
-            "| \033[1;33mNetwork height:\033[0m \033[1;36m$network_height\033[0m" \
-            "| \033[1;37mBlocks left:\033[0m \033[1;31m$blocks_left\033[0m"
-}
-
-run_story_node_setup() {
-    echo "Starting Story Node setup and checks..."
-    
-    install_story_node
-    echo "Waiting for services to start up..."
-    sleep 15 
-    
-    check_logs
-    echo "Logs checked. Waiting before sync status check..."
-    sleep 10  
-    
-    check_sync_status
-    echo "Setup and checks completed."
-}
-
-run_story_node_setup
+install_story_node
